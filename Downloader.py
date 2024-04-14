2c229ac66e5707fe57e4b1fab3e6567c77b3a965
Tips-white <167023596+Tips-white@users.noreply.github.com>
2024-04-14 20:00:28 +0200

    Create Downloader.py

---
+++ Downloader.py
@@ -0,0 +1,40 @@
+import requests
+import colorama 
+import os
+
+os.system(f"title Cwelium Downloader")
+
+stars = requests.get(f"https://api.github.com/repos/Tips-white/Cwelium-Raider").json()["stargazers_count"]
+
+menu = f"""
+{'██████╗  ██████╗ ██╗    ██╗███╗   ██╗██╗      ██████╗  █████╗ ██████╗ ███████╗██████╗ '.center(os.get_terminal_size().columns)}
+{'██╔══██╗██╔═══██╗██║    ██║████╗  ██║██║     ██╔═══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗'.center(os.get_terminal_size().columns)}
+{'██║  ██║██║   ██║██║ █╗ ██║██╔██╗ ██║██║     ██║   ██║███████║██║  ██║█████╗  ██████╔╝'.center(os.get_terminal_size().columns)}
+{'██║  ██║██║   ██║██║███╗██║██║╚██╗██║██║     ██║   ██║██╔══██║██║  ██║██╔══╝  ██╔══██╗'.center(os.get_terminal_size().columns)}
+{'██████╔╝╚██████╔╝╚███╔███╔╝██║ ╚████║███████╗╚██████╔╝██║  ██║██████╔╝███████╗██║  ██║'.center(os.get_terminal_size().columns)}
+{'╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝'.center(os.get_terminal_size().columns)}
+{f'Stars {stars}'.center(os.get_terminal_size().columns)}"""
+
+def download_latest_release():
+    api_url = f"https://api.github.com/repos/Tips-white/Cwelium-Raider/releases/latest"
+    
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        release_info = response.json()
+        asset_url = release_info['assets'][0]['browser_download_url']
+
+        response = requests.get(asset_url)
+        response.raise_for_status()
+
+        with open(f"Cwelium.exe", "wb") as file:
+            file.write(response.content)
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    print(menu)
+    download_latest_release()
+    os.system("start Cwelium.exe")
